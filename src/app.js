3d4ad676b9e7255dce239afea28dbdc940e35f7c
pranavgawande10 <pranavgawande05@gmail.com>
2026-01-02 18:51:38 +0530

    exploring get,put,delete,patch,post call

--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@ const express = require("express");
 
 const app = express();
 
+//this will only handle GET call to /user
+
+app.get("/user" , (req,res) => {
+    res.send({firstname : "pranav", lastname : "Gawande"});
+});
+
+app.post("/user" , (req,res) => {
+    res.send("data saved successfully in DB!");
+});
+
+app.delete("/user" , (req,res) => {
+    res.send("data deleted successfully!!");
+});
+
+app.put("/user" , (req, res) => {
+    res.send("Testing PUT call !!");
+});
+
+app.patch("/user" , (req,res) => {
+    res.send("TEsting PATCH call !!");
+});
+
+//this will match all the HTTP methods API call to /user
 app.use("/test", (req,res) => {
     res.send("hello from the pranav!....");
 });
 
-app.use("/hello", (req,res) => {
-    res.send("hello from the namaste node js!....");
-});
+
 
 app.listen(3000, () => {
     console.log("server is sucessfully listening at port 3000....");
