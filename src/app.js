fed23ee4acd4e920f56a322aa3cd0c648d942b54
pranavgawande10 <pranavgawande05@gmail.com>
2026-01-02 20:08:30 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -4,32 +4,34 @@ const app = express();
 
 //this will only handle GET call to /user
 
-app.get("/user" , (req,res) => {
-    res.send({firstname : "pranav", lastname : "Gawande"});
+// app.get("/a/:b/c" , (req,res) => {
+//     res.send({firstname : "pranav", lastname : "Gawande"});
+// });
+
+// app.get("/a/c" , (req,res) => {
+//     res.send({firstname : "pranav", lastname : "Gawande"});
+// });/
+// app.get(["/a/c", "/a/:b/c"], (req, res) => {
+//   res.send({ firstname: "pranav", lastname: "Gawande" });
+// });
+
+// app.get(/^\/ab+c$/, (req, res) => {
+//   res.send({ firstname: "pranav", lastname: "Gawande" });
+// });
+
+
+// app.get(/^\/ab*cd$/, (req, res) => {
+//   res.send({ firstname: "pranav", lastname: "Gawande" });
+// });
+
+// app.get(/^\/ab?cd$/, (req, res) => {
+//   res.send({ firstname: "pranav", lastname: "Gawande" });
+// });
+
+app.use("/user" , (req,res) => {
+    console.log(req.query);
+    res.send({firstname:"Pranav" , lastname: "Gawande"});
 });
-
-app.post("/user" , (req,res) => {
-    res.send("data saved successfully in DB!");
-});
-
-app.delete("/user" , (req,res) => {
-    res.send("data deleted successfully!!");
-});
-
-app.put("/user" , (req, res) => {
-    res.send("Testing PUT call !!");
-});
-
-app.patch("/user" , (req,res) => {
-    res.send("TEsting PATCH call !!");
-});
-
-//this will match all the HTTP methods API call to /user
-app.use("/test", (req,res) => {
-    res.send("hello from the pranav!....");
-});
-
-
 
 app.listen(3000, () => {
     console.log("server is sucessfully listening at port 3000....");
