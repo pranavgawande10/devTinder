80bbe7d0803f271a1bfbbf70e79ccce6030bd7ab
pranavgawande10 <pranavgawande05@gmail.com>
2026-01-03 15:34:43 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -28,10 +28,24 @@ const app = express();
 //   res.send({ firstname: "pranav", lastname: "Gawande" });
 // });
 
-app.use("/user" , (req,res) => {
-    console.log(req.query);
-    res.send({firstname:"Pranav" , lastname: "Gawande"});
-});
+// app.use("/user" , (req,res) => {
+//     console.log(req.query);
+//     res.send({firstname:"Pranav" , lastname: "Gawande"});
+// });
+app.use("/user" , (req,res,next) => {
+    console.log("handling the route user1 ");
+    //next();
+    //res.send("response 1!");
+    next();
+}, (req,res,next) => {
+    console.log("handling the route user2 ");
+    next();
+    res.send("response 2!"); });
+
+    //multiple route handlers!!!!
+    //syntax: app.use("/user" , RH1,RH2,RH3...);
+    //syntax: app.use("/user" , [RH1,RH2],RH3,RH4,RH5...); it is possible to encapsulate into array no problem or error occur!!!!!
+
 
 app.listen(3000, () => {
     console.log("server is sucessfully listening at port 3000....");
