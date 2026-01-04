137f85466985b0df96ff58d8f14bca7661952a59
pranavgawande10 <pranavgawande05@gmail.com>
2026-01-04 15:10:55 +0530

    wildcard-error handling-1

--- src/app.js
+++ src/app.js
@@ -2,54 +2,17 @@ const express = require("express");
 
 const app = express();
 
-const {userauth, adminauth} = require("./middlewares/auth.js");
-// app.get("/admin/getdata" , (req,res) =>{
-//     const auth = "abc";
-//     const isauth = auth === "abc";
+app.get("/user/getdata" , (req,res) =>{
 
-//     if(isauth)
-//     {
-//         res.send("sdmin data sent");
-//     }
-//     else{
-//         res.status(401).send("unauthorized request!");
-//     }
-// });
-// app.get("/user/data" ,(req,res)=>{
-//     const auth = "xyz";
-//     const isauth = auth === "xyz";
-
-//     if(isauth)
-//     {
-//         res.send("user data sent!!");
-//     }
-//     else{
-//         res.status(401).send("unauthorized request from user!!");
-//     }
-// });
-
-// app.use("/user" , (req,res,next)=>{
-//     const auth = "abcc";
-//     const isauth = auth === "abc";
-
-//     if(!isauth)
-//     {
-//         res.status(401).send("unauthorized request!");
-//     }
-//     else{
-//      next();
-//     }
-// });
-
-
-app.get("/user/data" ,userauth, (req,res)=>{
-    
-        res.send("user data sent!!");
-    
+    throw new Error("asfasdfa");
+    res.send("user data sent");
 });
-app.get("/admin/getdata" ,adminauth, (req,res) =>{
-    
-        res.send("admin data sent");
+
+app.use("/" ,(err,req,res)=>{
+    if(err)
+    {
+        res.status(500).send("Something went wrong!");
+    }
 });
 
 
